cb616fe645c97a8358cb066067e920377b8a4ae2
ScarSu <907866991@qq.com>
2020-07-23 18:19:39 +0800

    feat:添加暗黑模式按钮；添加ToTop按钮

--- js/background.js
+++ js/background.js
@@ -3,7 +3,6 @@ chrome.runtime.onInstalled.addListener(function(){
 		chrome.declarativeContent.onPageChanged.addRules([
 			{
 				conditions: [
-					// 只有打开百度才显示pageAction
 					new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlContains: 'notion.so'}})
 				],
 				actions: [new chrome.declarativeContent.ShowPageAction()]
--- js/content-script.js
+++ js/content-script.js
@@ -1,6 +1,10 @@
 var $notion=$("#notion-app")
 var notionFlag = $notion.length>0
+var darkBtnFlag = false
 
+window.scrollToTop = function scrollToTop(){
+  $(".notion-scroller").animate({scrollTop: 0}, 200, null);
+}
 if(notionFlag){
   var path = location.pathname;
 
@@ -47,6 +51,12 @@ if(notionFlag){
       return
     }
     $("#toc-btn-chrome-plugin").show();
+    var $li = $(`
+    <li class="level-1">
+      <a class="toTopBtn" href="#">TOP</a>
+    </li>
+    `)
+    $li.click(scrollToTop)
     var liStr=''
     for(var i=0,l=hs.length;i<l;i++){
       if(!hs[i].desc || !hs[i].level) continue
@@ -57,14 +67,56 @@ if(notionFlag){
       +'</li>'
     }
     $ul.html(liStr)
+    $ul.append($li)
   }
 
-  // 定时
+  // 定时刷新list
   setInterval(updateList,10000)
   setInterval(function checkUrl(){
     if(path !== location.pathname){
       updateList()
     }
   },1000)
+
+  window.onload=function(){
+    
+    // 在share按钮前添加暗黑模式按钮
+    var darkTimer = setInterval(function(){
+      if(darkBtnFlag){
+        clearInterval(darkTimer)
+      }else{
+        addDarkBtn()
+      }
+    },2000)
+  }
+
+  function addDarkBtn(){
+    var $btn_share = $(".notion-topbar-share-menu")
+    if($btn_share.length>0){
+      var $btn_dark = $(`
+        <div id="dark-btn-chrome-plugin" title="暗黑模式">
+          <input type="checkbox" id="dark-mode-inp"/>
+          <label for="dark-mode-inp"></label>
+        </div>
+      `)
+      $btn_dark.find("label").click(function(e){
+        e.stopPropagation()
+        var oldChecked = $(e.currentTarget).parent().find("input")[0].checked
+        if(!oldChecked){
+          turnOnDarkMode()
+        }else{
+          turnOffDarkMode()
+        }
+      })
+      $btn_dark.insertBefore($btn_share)
+      darkBtnFlag=true
+    }
+  }
+  function turnOnDarkMode(){
+    $('html').addClass('dark')
+  }
+  function turnOffDarkMode(){
+    $('html').removeClass('dark')
+  }
 
 }
