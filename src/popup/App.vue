98100e1000bc46b97fc4287ae6bfa573605bd625
scarsu <scarsu001@gmail.com>
2020-11-26 14:36:13 +0800

    feat(popup): update popup page

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -3,9 +3,10 @@
     <h2>NotionX</h2>
     <p>A Browser Extension that enhances Notion experience.</p>
     <div>
+      <a target="_blank" href="https://github.com/scarsu/NotionX">Homepage</a>;
+      <a target="_blank" href="https://github.com/scarsu/NotionX/issues/new">Feedback Here</a>;
       Open Source by
-      <a target="_blank" href="https://github.com/scarsu/NotionX">ScarSu</a>,
-      <a target="_blank" href="https://github.com/scarsu/NotionX/issues/new">Feedback Here</a>
+      <a target="_blank" href="https://github.com/scarsu/NotionX">ScarSu</a>;
     </div>
   </div>
 </template>
