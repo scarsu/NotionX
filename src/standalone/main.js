c137e0ef1f5e5fce2e7efee8860ec50e4191ee85
60057243 <ZengSuYu@aactechnologies.com>
2020-12-28 16:42:35 +0800

    refactor(standalone): generate index from README
    - 直接以readme作为项目主页
    - SSR
    - 避免维护多处项目信息

--- src/standalone/main.js
+++ src/standalone/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   render: (h) => h(App)
+// })
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  render: (h) => h(App)
+const path = require('path')
+const fs = require('fs')
+
+const mdContent = fs.readFileSync(path.resolve('./README.md'), 'utf-8')
+const md = require('markdown-it')()
+let _html = md.render(mdContent)
+const template = fs.readFileSync(path.resolve('./public/index.template.html'), 'utf8')
+_html = template.replace('<!--readme-md-outlet-->', _html)
+fs.writeFileSync(path.resolve('./dist/index.html'), _html, {
+  encoding: 'utf8',
+  mode: 0o666,
+  flags: 'w',
 })
+console.log(require('chalk').bgGreen(' Done ') + ' index.html generated from README.md successfully.')
